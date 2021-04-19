de21f35e162e4aa17211a8d5b9247e8be59672ec
frogofjuly <golubev.ki@phystech.edu>
2021-04-20 02:50:53 +0300

    up -> UP

--- python_src/test.py
+++ python_src/test.py
@@ -1,7 +1,7 @@
-import rogue_like as rl
+import roguelike as rl
 
 if __name__ == '__main__':
     g = rl.GameState()
     g.initialize(5)
-    g.receive_player_command(1, rl.up)
+    g.receive_player_command(1, rl.UP)
     print(g.get_serialization())
