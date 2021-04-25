0a70de2fd3cf51b81a243b29e3284bfdde2039fc
frogofjuly <golubev.ki@phystech.edu>
2021-04-25 22:19:28 +0300

    printing log from player

--- python_src/client.py
+++ python_src/client.py
@@ -9,7 +9,7 @@ import os
 
 
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 BUFFERSIZE = 2048
@@ -25,25 +25,28 @@ player_id = 0
 
 
 def render(game_state: dict):
+    global log
     cls()
-    if(game_state == {'start'}):
+    if game_state == {'start'}:
         return
     game_state = game_state['level']
     H = int(math.sqrt(len(game_state)))
     W = H
     player = None
+    newlog = None
     for tile_ in game_state:
         tile = tile_["tile"]
         if tile and "player" in tile:
             player = tile["player"]
+            newlog = player["lg_cpt"]["log"].split("\n")
             break
-    print(f"# Room: {1}/10") # TODO
-    print(f"# Score: {123:5}") # TODO
-    print('#'*(H+2)) # TODO
+    print(f"# Room: {1}/10")  # TODO
+    print(f"# Score: {123:5}")  # TODO
+    print('#' * (H + 2))  # TODO
     for i in range(H):
         print('#', end='')
         for j in range(W):
-            tile = game_state[i*W+j]['tile']
+            tile = game_state[i * W + j]['tile']
             if tile is None:
                 print(" ", end='')
             elif 'player' in tile:
@@ -51,8 +54,15 @@ def render(game_state: dict):
             elif 'entity' in tile:
                 print(tile['entity']['repr_cpt']['repr'], end='')
         print('#')
-    print('#'*(H+2))
+    print('#' * (H + 2))
     print(f'Level: {player["lvl"]:3}, Health: {player["h_cpt"]["health"]:3}, Damage: {player["a_cpt"]["damage"]}')
+    if not newlog:
+        return
+    print(f'log:')
+    for entry in newlog:
+        print(f'> {entry}')
+
+
 
 
 while True:
