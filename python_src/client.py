6b26b18f6b4e4d20b8a26074126bd9b9bc967aa9
krikun98 <krikun98@gmail.com>
2021-05-09 00:54:23 +0300

    accounting for future levels

--- python_src/client.py
+++ python_src/client.py
@@ -118,6 +118,8 @@ def render(stdscr, game_state: dict, flags: dict):
                     to_print = f' {repr} '
                     if 'h_cpt' in tile['entity'] and tile['entity']['h_cpt']['max_health'] > 0:
                         lvl = int(tile['entity']['h_cpt']['health'] * 10 / tile['entity']['h_cpt']['max_health'])
+                    elif 'lvl_cpt' in tile['entity']:
+                        lvl = tile['entity']['lvl_cpt']['level']
             if (lvl):
                 stdscr.addstr(i, j*3, to_print, curses.color_pair(color(lvl)))
             else:
