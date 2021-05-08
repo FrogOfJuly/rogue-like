bcba2356aa5d0b2cf5ca1c1caafe4e1e213af29d
frogofjuly <golubev.ki@phystech.edu>
2021-05-08 18:01:27 +0300

    log and decision cleaning separation fix

--- cpp_src/wrapper.cpp
+++ cpp_src/wrapper.cpp
@@ -27,7 +27,7 @@ PYBIND11_MODULE(roguelike, m) {
         .def("get_serialization", &roguelike::gamestate::get_serialization)
         .def("receive_player_command", &roguelike::gamestate::receive_player_command)
         .def("clean_logs", &roguelike::gamestate::clean_logs)
-        .def("clean_decisions", &roguelike::gamestate::clean_decision)
+        .def("clean_decisions", &roguelike::gamestate::clean_decisions)
         .def("end_turn", &roguelike::gamestate::end_turn)
         .def("decide_next_move", &roguelike::gamestate::decide_next_move);
 
